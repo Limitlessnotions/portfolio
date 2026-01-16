ca75c2673e8748449bbb07680115b63a5b26e9fe
Limitlessnotions <silasmomoh2017@gmail.com>
2026-01-16 10:53:04 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,11 +29,36 @@ export default function Header() {
           >
             Contact
           </a>
+
+          {/* Dark mode toggle */}
           <button
             onClick={() => setDark(!dark)}
-            className="underline underline-offset-4 transition-colors hover:text-neutral-900 dark:hover:text-neutral-200"
+            aria-label="Toggle dark mode"
+            className="transition-colors hover:text-neutral-900 dark:hover:text-neutral-200"
           >
-            {dark ? "Light" : "Dark"}
+            {dark ? (
+              /* Sun icon */
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 4.5a1 1 0 011 1V7a1 1 0 11-2 0V5.5a1 1 0 011-1zm0 11a3.5 3.5 0 100-7 3.5 3.5 0 000 7zm7.5-3.5a1 1 0 011-1H22a1 1 0 110 2h-1.5a1 1 0 01-1-1zM12 17a1 1 0 011 1v1.5a1 1 0 11-2 0V18a1 1 0 011-1zM3 11a1 1 0 011-1h1.5a1 1 0 110 2H4a1 1 0 01-1-1zm14.95-5.95a1 1 0 011.414 0l1.06 1.06a1 1 0 01-1.414 1.415l-1.06-1.06a1 1 0 010-1.415zM4.576 16.364a1 1 0 011.414 0l1.06 1.06a1 1 0 01-1.414 1.415l-1.06-1.06a1 1 0 010-1.415zm12.788 1.414a1 1 0 010-1.414l1.06-1.06a1 1 0 111.415 1.414l-1.06 1.06a1 1 0 01-1.415 0zM4.576 7.636a1 1 0 010-1.414l1.06-1.06a1 1 0 111.414 1.414l-1.06 1.06a1 1 0 01-1.414 0z" />
+              </svg>
+            ) : (
+              /* Moon icon */
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21.752 15.002A9 9 0 1112.998 2.25a7 7 0 108.754 12.752z" />
+              </svg>
+            )}
           </button>
         </nav>
       </div>
