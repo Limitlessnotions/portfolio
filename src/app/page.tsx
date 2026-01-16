a0e2fc766e0c2d5bcc225955e2634a63c7b962bf
Limitlessnotions <silasmomoh2017@gmail.com>
2026-01-16 08:59:00 +0100

    Render featured case studies on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import ProofBar from "../components/ProofBar";
 import Capabilities from "../components/Capabilities";
+import CaseStudies from "../components/CaseStudies";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <ProofBar />
       <Capabilities />
+      <CaseStudies />
     </main>
   );
 }
