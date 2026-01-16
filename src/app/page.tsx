cd077d5a5389c1530917a5c44ccf791e9ae9f668
Limitlessnotions <silasmomoh2017@gmail.com>
2026-01-16 08:39:09 +0100

    Render capabilities section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import ProofBar from "../components/ProofBar";
+import Capabilities from "../components/Capabilities";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <ProofBar />
+      <Capabilities />
     </main>
   );
 }
